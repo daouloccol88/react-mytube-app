f8411b90a18e385e4c2fd4e04678914209f19ae8
daouloccol88 <Daouda@iMac-de-codeloccol-83.local>
2020-06-18 04:19:46 +0100

    Updates

---
+++ precache-manifest.6949a581e137ebeb4cae595e8e9c4b65.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03b9019e47bd1276f2a7789b4ce5c5c0",
+    "url": "/react-mytube-app/index.html"
+  },
+  {
+    "revision": "148a48c4bd3b7f31cb5d",
+    "url": "/react-mytube-app/static/js/2.f1fb2511.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-mytube-app/static/js/2.f1fb2511.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7b66a1181f843c7bb56",
+    "url": "/react-mytube-app/static/js/main.75d64c24.chunk.js"
+  },
+  {
+    "revision": "9bea6d74c0f51e2b73e4",
+    "url": "/react-mytube-app/static/js/runtime-main.717ab0bd.js"
+  }
+]);
